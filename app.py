b47c0918d817dd56acfc7f8245bb58fbec4227c9
Areen Phadtare <phadtareareen@gmail.com>
2026-05-20 21:44:49 +0530

    Second  commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TODO.db'
@@ -12,12 +12,27 @@ class Todo(db.Model):
     completed = db.Column(db.Boolean, default=False)
  
 def __repr__(self):
-    return f'<Todo {self.title}>'
+      return f'<Todo {self.title} - {self.id}>'
 
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def hello():
+    if request.method=='POST':
+      print(request.form['title'])
+    todo=Todo(
+        title='Sample Task', 
+        description='This is a sample task.',
+        completed=False
+        )
+    db.session.add(todo)
+    db.session.commit()
     return render_template('index.html')
     #return 'Hello, World!'
+
+@app.route('/todos')
+def get_todos():
+    todos = Todo.query.all()
+    print(todos)
+    return render_template('index.html', todos=todos)
 if __name__ == '__main__':
     app.run(debug=True)
